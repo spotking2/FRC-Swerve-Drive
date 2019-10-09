eef707f7021685e6a001bfb3c2c36d8a685cb5af
spotking2 <35280670+spotking2@users.noreply.github.com>
2019-10-08 17:02:41 -0700

    Fixed math errors, added overloaded functions for testing

--- SRF_Swerve_Drive.java
+++ SRF_Swerve_Drive.java
@@ -4,13 +4,26 @@ import edu.wpi.first.wpilibj.Joystick;
 
 class SRF_Swerve_Drive {
     Joystick xBox;
-    double wheelBase,trackWidth,R;
+    double wheelBase,trackWidth,radius, gyroValue;
+    /*
+    wheelbase[]: Wheel number
+    wheelbase[][]: 0 - Speed, 1 - Angle
+    */
+    double[][] wheelValues = new double[4][2];
     
-    public SRF_Swerve_Drive(Joystick j, double w, double t, double r) {
+    public SRF_Swerve_Drive(Joystick j, double w, double t, double r, double gyro) {
         xBox = j;
         wheelBase = w;
         trackWidth = t;
-        R = r;
+        radius = r;
+        gyroValue = gyro;
+    }
+
+    public SRF_Swerve_Drive(double w, double t, double r, double gyro) {
+        wheelBase = w;
+        trackWidth = t;
+        radius = r;
+        gyroValue = gyro;
     }
 
     public double[] convertToFieldPosition(double Y, double X, double gyroAngle){
@@ -23,15 +36,63 @@ class SRF_Swerve_Drive {
         return new double[] {newY,newX};
     }
 
-    public void calculate(boolean orientToField){
-        double X, Y, W, TemporaryForGyro;
+    public double[][] calculate(boolean orientToField){
+        double X, Y, W, A, B, C, D;
         X = xBox.getRawAxis(0);
         Y = xBox.getRawAxis(1);
         W = xBox.getRawAxis(4);
         
         if(orientToField)
-            convertToFieldPosition(X,Y, TemporaryForGyro);
+            convertToFieldPosition(X,Y,gyroValue);
 
+        A = X - W*(wheelBase/radius);
+        B = X + W*(wheelBase/radius);
+        C = Y - W*(trackWidth/radius);
+        D = Y + W*(trackWidth/radius);
+
+        wheelValues[0][0] = Math.sqrt(Math.pow(B,2) + Math.pow(C,2));
+        wheelValues[1][0] = Math.sqrt(Math.pow(B,2) + Math.pow(D,2));
+        wheelValues[2][0] = Math.sqrt(Math.pow(A,2) + Math.pow(D,2));
+        wheelValues[3][0] = Math.sqrt(Math.pow(A,2) + Math.pow(C,2));
+
+        wheelValues[0][1] = Math.atan2(B,C)*(180/Math.PI);
+        wheelValues[1][1] = Math.atan2(B,D)*(180/Math.PI);
+        wheelValues[2][1] = Math.atan2(A,D)*(180/Math.PI);
+        wheelValues[3][1] = Math.atan2(A,C)*(180/Math.PI);
+
+        return wheelValues;
+    }
+
+    public double[][] calculate(boolean orientToField, double X, double Y, double W){
+        double A, B, C, D;
         
+        if(orientToField)
+            convertToFieldPosition(X,Y,gyroValue);
+
+        A = X - W*(wheelBase/radius);
+        B = X + W*(wheelBase/radius);
+        C = Y - W*(trackWidth/radius);
+        D = Y + W*(trackWidth/radius);
+
+        wheelValues[0][0] = Math.sqrt(Math.pow(B,2) + Math.pow(C,2));
+        wheelValues[1][0] = Math.sqrt(Math.pow(B,2) + Math.pow(D,2));
+        wheelValues[2][0] = Math.sqrt(Math.pow(A,2) + Math.pow(D,2));
+        wheelValues[3][0] = Math.sqrt(Math.pow(A,2) + Math.pow(C,2));
+
+        wheelValues[0][1] = Math.atan2(B,C)*(180/Math.PI);
+        wheelValues[1][1] = Math.atan2(B,D)*(180/Math.PI);
+        wheelValues[2][1] = Math.atan2(A,D)*(180/Math.PI);
+        wheelValues[3][1] = Math.atan2(A,C)*(180/Math.PI);
+
+        return wheelValues;
     }
 }
+
+
+
+
+
+
+
+
+
